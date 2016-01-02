88a563641bc6da821ed5b2abb4b6ce013dce99f5
Gideon Oduro <gideon.oduro@tradedoubler.com>
2016-01-02 15:15:34 +0100

    added helper functions

--- src/snake/core.clj
+++ src/snake/core.clj
@@ -2,6 +2,16 @@
   (:import [java.awt.event KeyEvent])
   (:use [quil.core :as q]))
 
+; add two vectors
+; (v+ [1 2] [1 1]) => [2 3]
+(def v+ (partial mapv +))
+
+; given a percent chance, decide if a event happens
+; (askoracle 0.5) => true
+; (askoracle 0.5) => false
+(defn askoracle [n]
+  (< (rand) n))
+
 ;update
 (defn update []
   )
