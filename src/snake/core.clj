616a355924ba26f0c809779a3a68edabc97f9f84
Gideon Oduro <gideon.oduro@tradedoubler.com>
2016-01-02 15:30:20 +0100

    drawing food on random position on map

--- src/snake/core.clj
+++ src/snake/core.clj
@@ -1,6 +1,9 @@
 (ns snake.core
   (:import [java.awt.event KeyEvent])
   (:use [quil.core :as q]))
+;*****************************************************
+;**************** Helper functions *******************
+;*****************************************************
 
 ; add two vectors
 ; (v+ [1 2] [1 1]) => [2 3]
@@ -12,14 +15,31 @@
 (defn askoracle [n]
   (< (rand) n))
 
+;*****************************************************
+;****************** Game functions *******************
+;*****************************************************
+
+; food (as set of points)
+(def food (atom #{}))
+
+;*****************************************************
+;****************** Core functions *******************
+;*****************************************************
+
 ;update
 (defn update []
+  ;spwan food
+  (when (askoracle 0.005)
+    (swap! food conj [(rand-int 450) (rand-int 200)]))
   )
 
 ;draw
 (defn draw []
   ; set background color to dark grey, draw color to with
   (q/background-float 0x20)
+  ;draw food
+  (q/stroke 0x00 0xff 0xff)
+  (doseq [[x y] @food] (q/point x y))
   )
 
 ;input
