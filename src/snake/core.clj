0e688ae9a08bfbaa6e05dc3f0068665addbf325e
Gideon Oduro <gideon.oduro@tradedoubler.com>
2016-01-02 16:24:56 +0100

    moving snakr not working

--- src/snake/core.clj
+++ src/snake/core.clj
@@ -22,12 +22,25 @@
 ; food (as set of points)
 (def food (atom #{}))
 
+; snake (as vector of points because we want to append)
+; => obviously needs a start position
+(def snake (atom [[50 50]]))
+
+; snake movement direction
+(def snake-dir (atom [1 0]))
+
+;We can calculate the new snake position
+;by adding the snake's head with the snake-dir:
+(v+ (first @snake) @snake-dir)
 ;*****************************************************
 ;****************** Core functions *******************
 ;*****************************************************
 
 ;update
 (defn update []
+  ;update snake
+  (let [new (v+ (first @snake) @snake-dir)] ; new head
+    (swap! snake #(vec (cons new (pop %))))) ; head+rest
   ;spwan food
   (when (askoracle 0.005)
     (swap! food conj [(rand-int 450) (rand-int 200)]))
@@ -37,13 +50,14 @@
 (defn draw []
   ; set background color to dark grey, draw color to with
   (q/background-float 0x20)
+  ;draw snake
+  (q/stroke 0xff)
+  (doseq [[x y] @snake] (q/point x y))
   ;draw food
   (q/stroke 0x00 0xff 0xff)
   ; we draw each meal by running through @food with doseq,
   ; where we use q/point to draw each point.
-  (doseq [[x y] @food]
-    (q/point x y))
-  )
+  (doseq [[x y] @food] (q/point x y)))
 
 ;input
 (defn key-pressed []
