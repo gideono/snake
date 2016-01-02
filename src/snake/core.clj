d502292ace07bd46d9635007060f7c67873b7abc
Gideon Oduro <gideon.oduro@tradedoubler.com>
2016-01-02 15:11:59 +0100

    core setup done

--- src/snake/core.clj
+++ src/snake/core.clj
@@ -1,6 +1,30 @@
-(ns snake.core)
+(ns snake.core
+  (:import [java.awt.event KeyEvent])
+  (:use [quil.core :as q]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+;update
+(defn update []
+  )
+
+;draw
+(defn draw []
+  ; set background color to dark grey, draw color to with
+  (q/background-float 0x20)
+  )
+
+;input
+(defn key-pressed []
+  )
+
+;run
+(q/defsketch snake
+             :title "Beach Boys Sweden 2d snake game"
+             :size [450 200]
+             :setup (fn []
+                      (q/smooth)
+                      (q/no-stroke)
+                      (q/frame-rate 60))
+             :draw (fn []
+                     (update)
+                     (draw))
+             :key-pressed key-pressed)
